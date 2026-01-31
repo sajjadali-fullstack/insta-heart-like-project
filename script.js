db420fe608772d05af086898307672b546798189
your sajju <sajjadali.dev01@gmail.com>
2026-01-31 09:37:10 +0530

    Initial commit - Insta Like Project

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-let cont = document.querySelector('.container')
-let like = document.querySelector('#like')
-// let heart = document.querySelector('#heart')
 
-cont.addEventListener('dblclick', () => {
-    console.log('You Like this post');
-    like.style.transform = 'translate(-50% -50%)scale(1);'
-    // like.style.color = 'red'
-    like.style.opacity = 0.9
+// cont.addEventListener('contextmenu', () => {
+// alert("Dont hack us by Right click Please!");
+// })
 
+let card = document.querySelector('#card')
+let heart = document.querySelector('i')
 
+card.addEventListener('dblclick',() => {
+    console.log('you liked it')
+    heart.style.transform = 'translate(-50%,-50%) scale(1)'
+    heart.style.opacity = 0.8
 
+    setTimeout(()=> {
+        heart.style.opacity = 0
+    },1000)
 
-
-    // Set Timeout in Seconds / Dealy Function inbuilt function
-
-    setTimeout(() => {
-        like.style.transform = 'translate(-50% -50%)scale(1);'
-        like.style.opacity = 0
-
-    }, 3000)
-
-
-    setTimeout(() => {
-        like.style.transform = 'transform ease 0.5s'
-        like.style.opacity = 0
-
-    }, 1000)
+    setTimeout(()=> {
+        heart.style.transform = 'translate(-50%,-50%) scale(0)'
+    }, 2000)
 })
 
-cont.addEventListener('contextmenu', () => {
-    alert("Dont hack us by Right click Please!");
-})
 
+
+// Set Timeout [concept of delay]
+
+setTimeout(()=> {
+    console.log('Hello1')
+}, 3000)
+
+setTimeout(() => {
+    console.log('Hello2')
+}, 2000)
